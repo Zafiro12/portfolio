eadd58b5c5539b064994e84bb462f89317c07400
Zafiro12 <alexsanta1205@gmail.com>
2024-11-16 12:52:37 +0100

    Add Vercel analytics and speed insights packages
    Updated package.json and package-lock.json to include @vercel/analytics and @vercel/speed-insights. Modified main.jsx to integrate these packages into the application.

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,27 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import TextScene from "./scenes/TextScene.jsx";
+import {Analytics} from "@vercel/analytics/react";
+import {SpeedInsights} from "@vercel/speed-insights/react";
 
 function Overlay() {
-  return (
-    <div className="absolute top-0 left-0 pointer-events-none w-full h-full">
-      <div className="absolute bottom-6 left-6">
-        Zafiro12
-        <br/>
-        Credit to <a href="https://pmnd.rs/" className="font-bold underline">pmnd.rs</a> for
-        the base code used.
-      </div>
-    </div>
-  )
+    return (
+        <div className="absolute top-0 left-0 pointer-events-none w-full h-full">
+            <div className="absolute bottom-6 left-6">
+                Zafiro12
+                <br/>
+                Credit to <a href="https://pmnd.rs/" className="font-bold underline">pmnd.rs</a> for
+                the base code used.
+            </div>
+        </div>
+    )
 }
 
 createRoot(document.getElementById('root')).render(
-  <>
-    <TextScene/>
-    <Overlay/>
-  </>
+    <>
+        <TextScene/>
+        <Overlay/>
+        <Analytics/>
+        <SpeedInsights/>
+    </>
 )
