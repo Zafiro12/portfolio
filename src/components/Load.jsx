d51b1aba0db73c1fbe74b05d560ebcb2888a7a04
Zafiro12 <alexsanta1205@gmail.com>
2024-11-22 18:54:43 +0100

    Replace TextScene with Load component and update assets
    Replaced TextScene with the new Load component, which includes an animated logo. Removed the old SVG file, added new SVG assets for logos in different colors, and updated some CSS for background color consistency. Renamed the footer credit to Alejandro Santaella.

---
+++ src/components/Load.jsx
@@ -0,0 +1,10 @@
+import Logo from "./Logo.jsx";
+
+export default function Load() {
+
+    return (
+        <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+            <Logo animate/>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import TextScene from "./scenes/TextScene.jsx";
 import {Analytics} from "@vercel/analytics/react";
 import {SpeedInsights} from "@vercel/speed-insights/react";
+import Load from "./components/Load.jsx";
 
 function Overlay() {
     return (
         <div className="absolute top-0 left-0 pointer-events-none w-full h-full">
-            <div className="absolute bottom-6 left-6">
-                Zafiro12
-                <br/>
-                Credit to <a href="https://pmnd.rs/" className="font-bold underline">pmnd.rs</a> for
-                the base code used.
+            <div className="absolute text-gray-500 bottom-6 left-6">
+                Alejandro Santaella
             </div>
         </div>
     )
@@ -19,7 +16,7 @@ function Overlay() {
 
 createRoot(document.getElementById('root')).render(
     <>
-        <TextScene/>
+        <Load/>
         <Overlay/>
         <Analytics/>
         <SpeedInsights/>
